1f05837d6b80044ad99fbcbd1309935dd6ad979d
Paulo De La O <paulodelao11@hotmail.com>
2025-12-09 20:57:09 -0600

    creacion  del modelo

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using InventarioInteligente.Models;
+
+namespace InventarioInteligente.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
